3e41f6f606914bba7fae54529e0c2738b718baca
Mauricio <mauriciojdsantos@gmail.com>
2015-11-11 19:46:30 -0200

    bd customization - case side effect

---
+++ src/main/java/org/msh/etbm/custom/bd/entities/CaseSideEffectBD.java
@@ -0,0 +1,131 @@
+package org.msh.etbm.custom.bd.entities;
+
+import org.msh.etbm.custom.bd.entities.enums.SideEffectAction;
+import org.msh.etbm.custom.bd.entities.enums.SideEffectGrading;
+import org.msh.etbm.custom.bd.entities.enums.SideEffectOutcome;
+import org.msh.etbm.custom.bd.entities.enums.SideEffectSeriousness;
+import org.msh.etbm.entities.CaseSideEffect;
+import org.msh.utils.date.DateUtils;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+@DiscriminatorValue("bd")
+public class CaseSideEffectBD extends CaseSideEffect {
+
+	private SideEffectGrading grade;
+	
+	private SideEffectAction actionTaken;
+	
+	private SideEffectOutcome outcome;
+	
+	private SideEffectSeriousness seriousness;
+	
+	@Temporal(TemporalType.DATE)
+	private Date effectSt;
+
+	@Temporal(TemporalType.DATE)
+	private Date effectEnd;
+	
+	//private String comments;	
+	
+	public Integer getMonthOfTreatment(){
+		//for registers of before the changing of month of treatment to iniDate 
+		if(getMonth() != 0)
+			return getMonth();
+		
+		if(getTbcase().getTreatmentPeriod() == null || effectSt == null)
+			return null;
+
+		int i = 0;
+		i = DateUtils.monthsBetween(getTbcase().getTreatmentPeriod().getIniDate(), effectSt)+1;
+		return i;
+	}
+		
+	public SideEffectGrading getGrade() {
+		return grade;
+	}
+
+	public void setGrade(SideEffectGrading grade) {
+		this.grade = grade;
+	}
+
+	public SideEffectAction getActionTaken() {
+		return actionTaken;
+	}
+
+	public void setActionTaken(SideEffectAction actionTaken) {
+		this.actionTaken = actionTaken;
+	}
+
+	public SideEffectOutcome getOutcome() {
+		return outcome;
+	}
+
+	public void setOutcome(SideEffectOutcome outcome) {
+		this.outcome = outcome;
+	}
+
+	public SideEffectSeriousness getSeriousness() {
+		return seriousness;
+	}
+
+	public void setSeriousness(SideEffectSeriousness seriousness) {
+		this.seriousness = seriousness;
+	}
+
+//	public String getComments() {
+//		return comments;
+//	}
+//
+//	public void setComments(String comments) {
+//		this.comments = comments;
+//	}
+
+	public Date getEffectSt() {
+		return effectSt;
+	}
+
+	public void setEffectSt(Date effectSt) {
+		this.effectSt = effectSt;
+	}
+
+	public Date getEffectEnd() {
+		return effectEnd;
+	}
+
+	public void setEffectEnd(Date effectEnd) {
+		this.effectEnd = effectEnd;
+	}
+
+	/**
+	 * Return month of treatment based on the start date of side effect
+	 * @return
+	 */
+	public String getIniMonthTreatmentDisplay() {
+		if (getEffectSt() == null)
+			return null;
+		return getTbcase().getTreatmentMonthDisplay(getEffectSt());
+	}
+	
+	/**
+	 * Return month of treatment based on the start treatment date and the collected date
+	 * @return
+	 */
+	public String getEndMonthTreatmentDisplay() {
+		if (getEffectEnd() == null)
+			return null;
+		return getTbcase().getTreatmentMonthDisplay(getEffectEnd());
+	}
+	
+	public boolean hasAditionalinfo(){
+		return ((getMedicines() != null && !getMedicines().trim().equals("")) 
+				|| getGrade() != null || getSeriousness() != null || getActionTaken() != null || 
+				getOutcome() != null ||	getEffectEnd() != null || (getComment() != null && !getComment().trim().equals("")));
+	}
+	
+}
