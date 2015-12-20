2a469fe1483c24810d0da2b0ca92e4458e8e060c
Mauricio <msantos.msh@gmail.com>
2015-12-20 15:53:22 -0200

    Avoid duplicity for sideeffect

--- src/main/java/org/msh/etbm/services/cases/SideEffectServices.java
+++ src/main/java/org/msh/etbm/services/cases/SideEffectServices.java
@@ -1,10 +1,28 @@
 package org.msh.etbm.services.cases;
 
+import org.msh.etbm.custom.bd.entities.CaseSideEffectBD;
+import org.msh.etbm.desktop.app.App;
 import org.msh.etbm.entities.CaseSideEffect;
+import org.msh.etbm.entities.SynchronizableEntity;
+import org.msh.etbm.entities.WSObject;
+import org.msh.etbm.entities.Workspace;
 import org.msh.etbm.services.core.EntityServicesImpl;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
+
 @Component
 public class SideEffectServices extends EntityServicesImpl<CaseSideEffect> {
-
+    /*
+	 * (non-Javadoc)
+	 *
+	 * @see org.msh.xview.VariableController#save(org.msh.xview.FormContext)
+	 */
+    @Override
+    public void save(CaseSideEffect entity) {
+        if(entity instanceof CaseSideEffectBD && entity != null){
+            entity.setMonth(entity.getTbcase().getMonthTreatment(((CaseSideEffectBD) entity).getEffectSt()));
+        }
+        super.save(entity);
+    }
 }
