8ce03ec28a513879bec7389bf8cdc384deb0391b
Ricardo Memoria <rmemoria@gmail.com>
2016-05-26 20:10:06 -0300

    pom.xml - creating a new development profile

---
+++ src/test/java/org/msh/etbm/test/desktop/TestTest.java
@@ -0,0 +1,14 @@
+package org.msh.etbm.test.desktop;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by rmemoria on 20/3/15.
+ */
+public class TestTest {
+
+    @Test
+    public void hiTest() {
+        System.out.println("Hi test");
+    }
+}
